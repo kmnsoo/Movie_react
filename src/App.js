c6f0eb9bee742abb9f909ddf47203eba8a6e31b2
kmnsoo <wkdska22@naver.com>
2021-11-09 11:47:01 +0900

    API로부터 받아온 State map()이용하여 보여주기

--- src/App.js
+++ src/App.js
@@ -15,10 +15,23 @@ function App() {
   useEffect(()=> {
   getMovies();    
   }, []);
-  console.log(movies);
   return (
     <div>
-      {loading ? <h1>Loading...</h1> : null}
+      {loading ? <h1>Loading...</h1> : ( 
+      <div>
+      {movies.map(movie => (
+      <div key={movie.id}>
+        <img src={movie.medium_cover_image} />
+        <h2>{movie.title}</h2>
+        <p>{movie.summary}</p>
+        <h4>Year: {movie.year}</h4>
+        <ul>
+          {movie.genres.map((genre) =>(<li key={genre}>{genre}</li>))}
+        </ul>
+      </div>
+      ))}
+      </div>
+      )}
       </div>
   );
 }
