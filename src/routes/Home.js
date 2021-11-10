5ff1c2fc84de6c69a810564786d30a63b05ed026
kmnsoo <wkdska22@naver.com>
2021-11-10 14:25:22 +0900

    CSS Styling 추가

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,15 @@
 import Movie from "../components/Movie";
 import {useEffect, useState} from 'react';
+import styles from "./Home.module.css";
+
 function Home() {
 
     const [loading ,setLoading] = useState(true);
-    const [movies, setMovies] =useState([])
-    const getMovies= async() =>{
+    const [movies, setMovies] =useState([]);
+    const getMovies= async () =>{
         const json = await (
             await fetch(
-       `https://yts-proxy.now.sh/list_movies.json`
+       `https://yts-proxy.now.sh/list_movies.json?minimum_rating=8.8&sort_by=like_count`
         )
     ).json();
             setMovies(json.data.movies);
@@ -18,18 +20,20 @@ function Home() {
        getMovies();
     }, []);
     return(
-        <div>
-            {loading ?  ( <h1>Loading...</h1>  ) : ( <div>{movies.map((movie) => (
-            <Movie 
-                key ={movie.id}
-                id= {movie.id}
-                coverImg = {movie.medium_cover_image} 
-                title={movie.title} 
-                summary={movie.summary} 
-                rating={movie.rating}
-                year = {movie.year}
-                genres={movie.genres}
-                />
+        <div className = {styles.container}>
+            {loading ?  ( <div className={styles.loader}> <span>Loading...</span></div>  ) : (  
+              <div className={styles.movies}>
+                {movies.map((movie) => (
+                 <Movie 
+                    key ={movie.id}
+                    id= {movie.id}
+                    coverImg = {movie.medium_cover_image} 
+                    title={movie.title} 
+                    summary={movie.summary} 
+                    rating={movie.rating}
+                    year = {movie.year}
+                    genres={movie.genres}
+                    />
              ))}
              </div>
              )}
